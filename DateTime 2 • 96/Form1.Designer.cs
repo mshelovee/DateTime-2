56838b5062f5d8cc128285ee19787a9b9e4485a4
halley <130781977+mshelovee@users.noreply.github.com>
2024-01-02 10:23:42 +0300

    Add files via upload

---
+++ DateTime 2 • 96/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace DateTime_2___96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.lblOdemeGunu = new System.Windows.Forms.Label();
+            this.lblTarih = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.Location = new System.Drawing.Point(12, 129);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(235, 49);
+            this.btnGoster.TabIndex = 11;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // lblOdemeGunu
+            // 
+            this.lblOdemeGunu.AutoSize = true;
+            this.lblOdemeGunu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOdemeGunu.Location = new System.Drawing.Point(18, 71);
+            this.lblOdemeGunu.Name = "lblOdemeGunu";
+            this.lblOdemeGunu.Size = new System.Drawing.Size(31, 24);
+            this.lblOdemeGunu.TabIndex = 10;
+            this.lblOdemeGunu.Text = "***";
+            // 
+            // lblTarih
+            // 
+            this.lblTarih.AutoSize = true;
+            this.lblTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTarih.Location = new System.Drawing.Point(18, 38);
+            this.lblTarih.Name = "lblTarih";
+            this.lblTarih.Size = new System.Drawing.Size(31, 24);
+            this.lblTarih.TabIndex = 9;
+            this.lblTarih.Text = "***";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(259, 190);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.lblOdemeGunu);
+            this.Controls.Add(this.lblTarih);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Label lblOdemeGunu;
+        private System.Windows.Forms.Label lblTarih;
+    }
+}
+
---
+++ DateTime 2 • 96/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTime_2___96
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            DateTime bugun = DateTime.Today;
+
+            DateTime odemeGunu = new DateTime(2022, 8, 25);
+
+
+            lblTarih.Text = "Tarih: " + bugun.ToShortDateString();
+            lblOdemeGunu.Text = "Ödeme Günü: " + odemeGunu.ToShortDateString();
+        }
+    }
+}
